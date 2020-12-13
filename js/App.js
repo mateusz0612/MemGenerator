a31da0db42b33f2a29a0071b907c8586607610f6
mateusz0612 <dennyfacio@gmail.com>
2020-12-14 00:02:32 +0100

    working with boxes api param

--- js/App.js
+++ js/App.js
@@ -9,14 +9,11 @@ const init = async () => {
   const data = await getMemesApi.getData();
   const memeUi = new MemeUI(DOMelements.memesContainer, data);
   memeUi.displayMemes();
-  const generateButton = new Button("Generete Meme!", () => {
-    const params = {};
-    Input.getInputsValue(".meme-text-input").forEach((e, index) => {
-      params[`text${index}`] = e;
+  const generateButton = new Button("Generete Meme!", async () => {
+    const boxes = [];
+    Input.getInputsValue(".meme-text-input").forEach((e) => {
+      boxes.push({ text: e });
     });
-    let generatedMemeUrl = `https://api.imgflip.com/caption_image&templete_id=${DOMelements.memeField.dataset.id}&username=dennni&pasword=kwasnejapko12`;
-    for (const [key, value] of Object.entries(params))
-      generatedMemeUrl += `&${key}=${value}`;
   });
   DOMelements.generateBtnContainer.appendChild(
     generateButton.createButton("meme-creator_generate_button")
