a459badbc06de2004c7edb9a895e5ef0646152ba
mateusz0612 <dennyfacio@gmail.com>
2020-12-14 21:30:31 +0100

    Working app

--- js/App.js
+++ js/App.js
@@ -5,7 +5,9 @@ import { Button } from "./Button.js";
 import { Input } from "./Input.js";
 
 const init = async () => {
-  const getMemesApi = new ApiHandler("https://api.imgflip.com/get_memes");
+  const getMemesApi = new ApiHandler(
+    "https://secret-ocean-49799.herokuapp.com/https://api.imgflip.com/get_memes"
+  );
   const data = await getMemesApi.getData();
   const memeUi = new MemeUI(DOMelements.memesContainer, data);
   memeUi.displayMemes();
@@ -17,13 +19,13 @@ const init = async () => {
     const formData = new FormData();
     formData.append("username", "dennni");
     formData.append("password", "kwasnejapko12");
-    formData.append("template_id", DOMelements.memeField.src);
+    formData.append("template_id", DOMelements.memeField.dataset.id);
     params.forEach((e, index) => formData.append(`boxes[${index}][text]`, e));
     const generateMemeApi = new ApiHandler(
       "https://api.imgflip.com/caption_image"
     );
     const generatedMeme = await generateMemeApi.getData(formData);
-    console.log(generatedMeme);
+    DOMelements.memeField.src = generatedMeme.data.url;
   });
   DOMelements.generateBtnContainer.appendChild(
     generateButton.createButton("meme-creator_generate_button")
